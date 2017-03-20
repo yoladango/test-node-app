5c134def3915eb77f1d76ec1e9bf6511f193aafb
yoladango <yoladango@gmail.com>
2017-03-19 20:58:51 -0700

    nother commit

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-var http = require('http');
+/*var http = require("http");
 
 http.createServer(function (request, response) {
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	response.end('Hello World\n');
 }).listen(8081);
 
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at http://127.0.0.1:8081/');*/
+
+/*var fs = require("fs");
+
+var data = fs.readFileSync('input.txt');
+console.log(data.toString());
+
+fs.readFile('input.txt', function(err, data) {
+	if (err) return console.error(err);
+	console.log(data.toString());
+})
+
+console.log('Program ended');*/
+
+var events = require("events");
+
+var eventEmitter = new events.EventEmitter
+
 
