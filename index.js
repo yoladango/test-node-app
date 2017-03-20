673b6c9e928c6cdcaa38d2fe8f1695f45b16935a
yoladango <yoladango@gmail.com>
2017-03-19 23:21:41 -0700

    ssss

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*var http = require("http");
+/*var http = require('http');
 
 http.createServer(function (request, response) {
 	response.writeHead(200, {'Content-Type': 'text/plain'});
@@ -7,7 +7,7 @@ http.createServer(function (request, response) {
 
 console.log('Server running at http://127.0.0.1:8081/');*/
 
-/*var fs = require("fs");
+/*var fs = require('fs');
 
 var data = fs.readFileSync('input.txt');
 console.log(data.toString());
@@ -15,14 +15,50 @@ console.log(data.toString());
 fs.readFile('input.txt', function(err, data) {
 	if (err) return console.error(err);
 	console.log(data.toString());
-})
+});
 
 console.log('Program ended');*/
 
-var events = require('events');
+/*var events = require('events');
 
 var eventEmitter = new events.EventEmitter();
 
-eventEmitter.on
+var connectHandler = function connected() {
+	console.log('connection successful');
+	eventEmitter.emit('data_received');
+};
+
+eventEmitter.on('connection', connectHandler);
+
+eventEmitter.on('data_received', function() {
+	console.log('data received successfully');
+	
+});
+
+eventEmitter.emit('connection');
+
+console.log('Program ended');*/
+
+/*var fs = require('fs');
+
+// Asynchronous read
+fs.readFile('input.txt', function (err, data) {
+	if (err) {
+		return console.error(err);
+	}
+	console.log('Asynchronous read: ' + data.toString());
+	
+});
+console.log('Asynchronous read: Program Ended');
+
+// Synchronous read
+var data = fs.readFileSync('input.txt');
+console.log('Synchronous read: ' + data.toString());
+
+console.log('Synchronous read: Program Ended');*/
+
+console.log(__filename);
+
+
 
 
